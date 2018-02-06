59cd518d72f4105e6cea8ea7294a191859f5b25d
Humna Awan <humna.awan@rutgers.edu>
2018-02-05 20:11:48 -0500

    Updates: dithered FOV now in pixels; undithered hollow; ra, dec in degrees; ra increases left. #108

--- scripts/visitGen/DC2_visitsSim.py
+++ scripts/visitGen/DC2_visitsSim.py
@@ -1,11 +1,11 @@
 # needs set up beforehand: code from https://github.com/humnaawan/DC1-Regions
-
 import os
 import numpy as np
 import healpy as hp
-from DC1code.intermediates import findFOVPixels
 import imageio
 import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 def pixNum2RaDec(nside, pix):
     """
@@ -18,7 +18,7 @@ def DC2VisitsSim(outDir, dataTag, simdata, pointingRACol, pointingDecCol,
                  obsIDsList, fIDsList, bandList,
                  regionPixels_WFD, regionPixels_DD,
                  nside,
-                 obsHistIndMin, obsHistIndMax,
+                 obsHistIndMin, obsHistIndMax, dpi= 40,
                  minRA= None, maxRA= None, minDec= None, maxDec= None):
     """
     Code to produce a GIF.
@@ -41,12 +41,7 @@ def DC2VisitsSim(outDir, dataTag, simdata, pointingRACol, pointingDecCol,
     radius= 0.0305
     delx= radius*np.cos(theta)
     dely= radius*np.sin(theta)
-
-    # find pixels related to each FOV for plotting purposes.
-    FOVpixs= {}
-    for fid in np.unique(fIDsList):
-        centralRA, centralDec, FOVpixs[fid]= findFOVPixels(fid, simdata, nside= nside,
-                                                            FOV_radius= radius)    
+    
     filenames= []  # keep track for imageio
     ###########################################################################
     # loop over histIDs
@@ -54,50 +49,64 @@ def DC2VisitsSim(outDir, dataTag, simdata, pointingRACol, pointingDecCol,
         fig, axes= plt.subplots(1,1)
         # plot the full protoDc2 region
         DC2pixRA, DC2pixDec= pixNum2RaDec(regionPixels_WFD, nside)
-        axes.plot(DC2pixRA, DC2pixDec, 'o', color= 'g', alpha= 0.3, ms= 10, label= 'WFD footprint')
+        axes.plot(np.degrees(DC2pixRA), np.degrees(DC2pixDec), 'o',
+                  color= 'g', alpha= 0.3, ms= 10, label= 'WFD footprint')
         # plot the uDDF region
         DC2pixRA, DC2pixDec= pixNum2RaDec(regionPixels_DD, nside)
-        axes.plot(DC2pixRA, DC2pixDec, 'o', color= 'b', alpha= 0.3, ms= 10, label= 'uDDF footprint')
+        axes.plot(np.degrees(DC2pixRA), np.degrees(DC2pixDec), 'o',
+                  color= 'b', alpha= 0.3, ms= 10, label= 'uDDF footprint')
         
         # ok. now deal with the individual visit.
         i= np.where(obsIDsList==hid)[0][0]
 
-        # plot the undithered FOV pixels
-        fid= fIDsList[i]
-        pixRA, pixDec= pixNum2RaDec(FOVpixs[fid], nside)
-        axes.plot(pixRA, pixDec, '.', color= 'k', ms= 10, alpha= 0.5, label='undithered FOV')
-
-        # dithered FOV
+        # --
+        # plot the undithered FOV with a circular boundary with an x at the center
         ind= np.where(simdata['obsHistID']==hid)[0] # all the entries corresponding to the objID.
+        pointingRA= simdata[ind]['fieldRA'] # radians
+        pointingDec= simdata[ind]['fieldDec'] # radians
+
+        # plot the undithered FOV boundary/center.
+        #axes.plot(np.degrees(pointingRA), np.degrees(pointingDec), 'x',
+        #          alpha= 0.5,
+        #          color= 'k',  mew= 2, ms= 10)
+        axes.plot(np.degrees(pointingRA+delx/np.cos(pointingDec)),
+                  np.degrees(pointingDec+dely), ':',
+                  alpha= 0.5,
+                  color= 'k',lw= 3, label= 'undithered FOV')
+        # --
+        
+        # plot the dithered FOV pixels
         pointingRA= simdata[ind][pointingRACol] # radians
         pointingDec= simdata[ind][pointingDecCol] # radians
-
-        # plot the FOV boundary/center.    
-        axes.plot(pointingRA, pointingDec, 'x', color= 'k',  mew= 2, ms= 10)
-        axes.plot(pointingRA+delx/np.cos(pointingDec), pointingDec+dely, color= 'k',lw= 3, label= 'dithered FOV')
+        c = SkyCoord(ra=pointingRA*u.radian, dec= pointingDec*u.radian)
+        regionPixels= hp.query_disc(nside= nside, vec=c.cartesian.xyz, radius= radius)
+        pixRA, pixDec= pixNum2RaDec(regionPixels, nside)
+        axes.plot(np.degrees(pixRA), np.degrees(pixDec), '.',
+                  color= 'k', ms= 10, alpha= 0.7, label='dithered FOV')
 
         # set up for title: include histIS, filter, rot angle
         title= '%s\nhID: %s ; fid: %s ; filter: %s'%(dataTag, obsIDsList[i], fIDsList[i], bandList[i])
         axes.set_title(title,  fontsize= fontsize, )
 
         # axes labels
-        axes.set_xlabel('RA (rad)', fontsize= fontsize)
-        axes.set_ylabel('Dec (rad)', fontsize= fontsize)
+        axes.set_xlabel('RA (deg)', fontsize= fontsize)
+        axes.set_ylabel('Dec (deg)', fontsize= fontsize)
 
         # axes limits
-        axes.set_xlim([np.radians(minRA-1), np.radians(maxRA+1)])
-        axes.set_ylim([np.radians(minDec-1), np.radians(maxDec+1)])
+        axes.set_xlim([minRA-1, maxRA+1])
+        axes.set_ylim([minDec-1, maxDec+1])
         #axes.axis('equal')
         
+        axes.invert_xaxis()
         axes.tick_params(axis='x', labelsize=fontsize)
         axes.tick_params(axis='y', labelsize=fontsize)
 
-        axes.legend(loc= "upper right", fontsize= fontsize)
+        axes.legend(loc= "lower left", fontsize= fontsize)
         fig.set_size_inches(10,10)   
         
         # save each png.
         filename= '%sth.png'%index
-        plt.savefig(filename, format= 'png', bbox_inches = 'tight', dpi= 40)
+        plt.savefig(filename, format= 'png', bbox_inches = 'tight', dpi= dpi)
         filenames.append(filename)
         plt.close()
 
