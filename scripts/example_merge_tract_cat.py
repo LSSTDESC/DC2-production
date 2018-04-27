691c65e047a913752b0a65f3ff0d714032294a2f
Michael Wood-Vasey <wmwv@pitt.edu>
2018-04-27 16:40:09 -0400

    Explain expected failures in example when writing HDF5, FITS.

--- scripts/example_merge_tract_cat.py
+++ scripts/example_merge_tract_cat.py
@@ -7,5 +7,9 @@ patch_cat = example_load_patch(tract=tract, patch=patch)
 
 filebase = 'merged_tract_%d_%s' % (tract, patch)
 patch_cat.write(filebase+'.ecsv', format='ascii.ecsv')
+
+# Each of the following is expected to fail:
+### Column names too long for FITS file standard
 patch_cat.write(filebase+'.fits', format='fits')
-# patch_cat.to_pandas().to_hdf(filebase+'.h5', 'table')
+### No PyTables available on Cori:
+patch_cat.to_pandas().to_hdf(filebase+'.h5', 'table')
