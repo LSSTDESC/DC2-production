e75de22762bc37b7ea770b233b16412c953769fe
Anže SlosarÄ <anze@bnl.gov>
2018-06-20 12:53:47 -0700

    input catalog reader cleanup + notebook

--- scripts/reader/input_catalog_reader.py
+++ scripts/reader/input_catalog_reader.py
@@ -13,12 +13,20 @@ from pandas.util.testing import assert_frame_equal
 
 __all__ = ['InputCatalogReader']
 
-class InputCatalogReader(BaseGenericCatalog):
-    
+class InputCatalogReader(BaseGenericCatalog):    
+    """Input catalog reader.
+    Optinal options:
+       filename -- which file to use
+       nlines -- how many lines to read at once
+   """
+
     def _subclass_init(self, **kwargs):
-        #fileName = '/global/projecta/projectdirs/lsst/groups/SSim/DC2/reference_catalogs/dc2_reference_catalog_dc2v3_fov4.txt'
-        fileName = '/global/homes/b/bhairav/dc2_test.txt'
-        self._file = fileName
+        self._filename = '/global/projecta/projectdirs/lsst/groups/SSim/DC2/reference_catalogs/dc2_reference_catalog_dc2v3_fov4.txt'
+        self._Nlines=10000
+        if 'filename' in kwargs:
+            self._filename=kwargs['filename']
+        if 'nlines' in kwargs:
+            self._Nlines=kwargs['nlines']
         self._quantity_modifiers = {
             'uniqueId' : 'uniqueId',
             'ra' : 'raJ2000',
@@ -52,7 +60,6 @@ class InputCatalogReader(BaseGenericCatalog):
             'parallax' : 'parallax',
             'radialVelocity' : 'radialVelocity'
         }
-        self.Nlines=10000
         
         self.native_quantities = ['uniqueId', 'raJ2000', 'decJ2000', 'sigma_raJ2000', 'sigma_decJ2000', 
                                   'raJ2000_smeared', 'decJ2000_smeared', 'lsst_u', 'sigma_lsst_u', 
@@ -69,12 +76,12 @@ class InputCatalogReader(BaseGenericCatalog):
             
     def _iter_native_dataset(self, native_filters = None):
         assert (native_filters is None)
-        with open(self._file, 'r') as f:
+        with open(self._filename, 'r') as f:
             for i in range(11):
                 f.readline()
             while True:
                 l=[]
-                for c in range(self.Nlines):
+                for c in range(self._Nlines):
                     line = f.readline()
                     if len(line) == 0:
                         break
@@ -87,13 +94,12 @@ class InputCatalogReader(BaseGenericCatalog):
                 
                 def native_quantity_getter(native_quantity):
                     res=np.array([values[:,self.name2ndx[native_quantity]]])
-                    #print(res)
                     return res.flatten()
                 yield native_quantity_getter
         
     
     def _generate_native_quantity_list(self, native_filters = None):
-        with open(self._file, 'r') as f:
+        with open(self._filename, 'r') as f:
             # skip 10 lines
             for i in range(10):
                 f.readline()
@@ -102,16 +108,7 @@ class InputCatalogReader(BaseGenericCatalog):
             fields = fields[1:] ### get rid of the first character
             fields = fields.split(', ')
             fields[len(fields) - 1] = fields[len(fields) - 1].strip()
-            fieldsData = pd.DataFrame({'data':fields})
-            nativeQuantData = pd.DataFrame({'data':self.native_quantities})
-        #assert fields == self.native_quantities, "The lists are not equal."
-        #print(assert all([a == b for a, b in zip(fields, self.native_quantities)]))
-        #for n1,n2 in zip(fields, self.native_quantities):
-        #assert(n1 == n2)
-        try:
-            assert_frame_equal(fieldsData.sort_index(axis=1), nativeQuantData.sort_index(axis=1), check_names=True)
-            print("Both frames are equal.")
-        except:
-            print("Both frames are not equal.")
+        for fc,mc in zip(fields,self.native_quantities):
+            assert(fc==mc)
         return self.native_quantities
-    
+
