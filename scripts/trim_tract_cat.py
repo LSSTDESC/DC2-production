8e36260bd9d3a6fbcf7085883c704008d30e9733
Michael Wood-Vasey <wmwv@pitt.edu>
2018-07-12 14:39:59 -0400

    Make columns_to_keep list unique.

--- scripts/trim_tract_cat.py
+++ scripts/trim_tract_cat.py
@@ -6,52 +6,58 @@ that contains only the columns necessary to support the DPDD
 columns exposed in the GCRCatalog DC2 reader
 """
 
+import os
+import sys
 
 import pandas as pd
 
+# The above list was generated 'generate_columns_to_keep'
+# But we don't want to regenerate this each time we run this script
+# so we cache the result here.
 columns_to_keep = \
-['id', 'parent', 'coord_ra', 'coord_dec', 'base_SdssCentroid_x',
-'base_SdssCentroid_y', 'base_SdssCentroid_xSigma', 'base_SdssCentroid_ySigma', 'base_SdssCentroid_flag',
-'base_PsfFlux_area', 'base_ClassificationExtendedness_value', 'base_Blendedness_abs_flux',
-'base_PixelFlags_flag_edge', 'base_PixelFlags_flag_interpolatedCenter', 'base_PixelFlags_flag_saturatedCenter',
-'base_PixelFlags_flag_crCenter', 'base_PixelFlags_flag_bad', 'base_PixelFlags_flag_suspectCenter',
-'base_PixelFlags_flag_clipped', 'deblend_skipped', 'base_PixelFlags_flag_edge',
-'base_PixelFlags_flag_interpolatedCenter', 'base_PixelFlags_flag_saturatedCenter', 'base_PixelFlags_flag_crCenter',
-'base_PixelFlags_flag_bad', 'base_PixelFlags_flag_suspectCenter', 'base_PixelFlags_flag_clipped',
-'ext_shapeHSM_HsmSourceMoments_flag', 'ext_shapeHSM_HsmSourceMoments_xx', 'base_SdssShape_psf_xx',
-'ext_shapeHSM_HsmSourceMoments_yy', 'base_SdssShape_psf_yy', 'ext_shapeHSM_HsmSourceMoments_xy',
-'base_SdssShape_psf_xy', 'y_mag', 'y_mag_err', 'y_base_PsfFlux_flux', 'y_base_PsfFlux_flag',
-'y_base_PsfFlux_fluxSigma', 'y_base_SdssShape_flag', 'y_base_SdssShape_xx', 'y_base_SdssShape_psf_xx',
-'y_base_SdssShape_yy', 'y_base_SdssShape_psf_yy', 'y_base_SdssShape_xy', 'y_base_SdssShape_psf_xy',
-'y_modelfit_CModel_flux', 'y_modelfit_CModel_flux', 'y_modelfit_CModel_fluxSigma', 'y_modelfit_CModel_flux',
-'y_modelfit_CModel_fluxSigma', 'y_base_SdssShape_psf_xx', 'y_base_SdssShape_psf_yy', 'y_base_SdssShape_psf_xy', 'g_mag',
-'g_mag_err', 'g_base_PsfFlux_flux', 'g_base_PsfFlux_flag', 'g_base_PsfFlux_fluxSigma', 'g_base_SdssShape_flag',
-'g_base_SdssShape_xx', 'g_base_SdssShape_psf_xx', 'g_base_SdssShape_yy', 'g_base_SdssShape_psf_yy',
-'g_base_SdssShape_xy', 'g_base_SdssShape_psf_xy', 'g_modelfit_CModel_flux', 'g_modelfit_CModel_flux',
-'g_modelfit_CModel_fluxSigma', 'g_modelfit_CModel_flux', 'g_modelfit_CModel_fluxSigma', 'g_base_SdssShape_psf_xx',
-'g_base_SdssShape_psf_yy', 'g_base_SdssShape_psf_xy', 'r_mag', 'r_mag_err', 'r_base_PsfFlux_flux',
-'r_base_PsfFlux_flag', 'r_base_PsfFlux_fluxSigma', 'r_base_SdssShape_flag', 'r_base_SdssShape_xx',
-'r_base_SdssShape_psf_xx', 'r_base_SdssShape_yy', 'r_base_SdssShape_psf_yy', 'r_base_SdssShape_xy',
-'r_base_SdssShape_psf_xy', 'r_modelfit_CModel_flux', 'r_modelfit_CModel_flux', 'r_modelfit_CModel_fluxSigma',
-'r_modelfit_CModel_flux', 'r_modelfit_CModel_fluxSigma', 'r_base_SdssShape_psf_xx', 'r_base_SdssShape_psf_yy',
-'r_base_SdssShape_psf_xy', 'z_mag', 'z_mag_err', 'z_base_PsfFlux_flux', 'z_base_PsfFlux_flag',
-'z_base_PsfFlux_fluxSigma', 'z_base_SdssShape_flag', 'z_base_SdssShape_xx', 'z_base_SdssShape_psf_xx',
-'z_base_SdssShape_yy', 'z_base_SdssShape_psf_yy', 'z_base_SdssShape_xy', 'z_base_SdssShape_psf_xy',
-'z_modelfit_CModel_flux', 'z_modelfit_CModel_flux', 'z_modelfit_CModel_fluxSigma', 'z_modelfit_CModel_flux',
-'z_modelfit_CModel_fluxSigma', 'z_base_SdssShape_psf_xx', 'z_base_SdssShape_psf_yy', 'z_base_SdssShape_psf_xy', 'i_mag',
-'i_mag_err', 'i_base_PsfFlux_flux', 'i_base_PsfFlux_flag', 'i_base_PsfFlux_fluxSigma', 'i_base_SdssShape_flag',
-'i_base_SdssShape_xx', 'i_base_SdssShape_psf_xx', 'i_base_SdssShape_yy', 'i_base_SdssShape_psf_yy',
-'i_base_SdssShape_xy', 'i_base_SdssShape_psf_xy', 'i_modelfit_CModel_flux', 'i_modelfit_CModel_flux',
-'i_modelfit_CModel_fluxSigma', 'i_modelfit_CModel_flux', 'i_modelfit_CModel_fluxSigma', 'i_base_SdssShape_psf_xx',
-'i_base_SdssShape_psf_yy', 'i_base_SdssShape_psf_xy', 'u_mag', 'u_mag_err', 'u_base_PsfFlux_flux',
-'u_base_PsfFlux_flag', 'u_base_PsfFlux_fluxSigma', 'u_base_SdssShape_flag', 'u_base_SdssShape_xx',
-'u_base_SdssShape_psf_xx', 'u_base_SdssShape_yy', 'u_base_SdssShape_psf_yy', 'u_base_SdssShape_xy',
-'u_base_SdssShape_psf_xy', 'u_modelfit_CModel_flux', 'u_modelfit_CModel_flux', 'u_modelfit_CModel_fluxSigma',
-'u_modelfit_CModel_flux', 'u_modelfit_CModel_fluxSigma', 'u_base_SdssShape_psf_xx', 'u_base_SdssShape_psf_yy',
-'u_base_SdssShape_psf_xy']
+['base_Blendedness_abs_flux', 'base_ClassificationExtendedness_value',
+ 'base_PixelFlags_flag_bad', 'base_PixelFlags_flag_clipped',
+ 'base_PixelFlags_flag_crCenter', 'base_PixelFlags_flag_edge',
+ 'base_PixelFlags_flag_interpolatedCenter',
+ 'base_PixelFlags_flag_saturatedCenter',
+ 'base_PixelFlags_flag_suspectCenter', 'base_PsfFlux_area',
+ 'base_SdssCentroid_flag', 'base_SdssCentroid_x', 'base_SdssCentroid_xSigma',
+ 'base_SdssCentroid_y', 'base_SdssCentroid_ySigma', 'base_SdssShape_psf_xx',
+ 'base_SdssShape_psf_xy', 'base_SdssShape_psf_yy', 'coord_dec', 'coord_ra',
+ 'deblend_skipped', 'ext_shapeHSM_HsmSourceMoments_flag',
+ 'ext_shapeHSM_HsmSourceMoments_xx', 'ext_shapeHSM_HsmSourceMoments_xy',
+ 'ext_shapeHSM_HsmSourceMoments_yy', 'g_base_PsfFlux_flag',
+ 'g_base_PsfFlux_flux', 'g_base_PsfFlux_fluxSigma', 'g_base_SdssShape_flag',
+ 'g_base_SdssShape_psf_xx', 'g_base_SdssShape_psf_xy',
+ 'g_base_SdssShape_psf_yy', 'g_base_SdssShape_xx', 'g_base_SdssShape_xy',
+ 'g_base_SdssShape_yy', 'g_mag', 'g_mag_err', 'g_modelfit_CModel_flux',
+ 'g_modelfit_CModel_fluxSigma', 'i_base_PsfFlux_flag', 'i_base_PsfFlux_flux',
+ 'i_base_PsfFlux_fluxSigma', 'i_base_SdssShape_flag',
+ 'i_base_SdssShape_psf_xx', 'i_base_SdssShape_psf_xy',
+ 'i_base_SdssShape_psf_yy', 'i_base_SdssShape_xx', 'i_base_SdssShape_xy',
+ 'i_base_SdssShape_yy', 'i_mag', 'i_mag_err', 'i_modelfit_CModel_flux',
+ 'i_modelfit_CModel_fluxSigma', 'id', 'parent', 'r_base_PsfFlux_flag',
+ 'r_base_PsfFlux_flux', 'r_base_PsfFlux_fluxSigma', 'r_base_SdssShape_flag',
+ 'r_base_SdssShape_psf_xx', 'r_base_SdssShape_psf_xy',
+ 'r_base_SdssShape_psf_yy', 'r_base_SdssShape_xx', 'r_base_SdssShape_xy',
+ 'r_base_SdssShape_yy', 'r_mag', 'r_mag_err', 'r_modelfit_CModel_flux',
+ 'r_modelfit_CModel_fluxSigma', 'u_base_PsfFlux_flag', 'u_base_PsfFlux_flux',
+ 'u_base_PsfFlux_fluxSigma', 'u_base_SdssShape_flag',
+ 'u_base_SdssShape_psf_xx', 'u_base_SdssShape_psf_xy',
+ 'u_base_SdssShape_psf_yy', 'u_base_SdssShape_xx', 'u_base_SdssShape_xy',
+ 'u_base_SdssShape_yy', 'u_mag', 'u_mag_err', 'u_modelfit_CModel_flux',
+ 'u_modelfit_CModel_fluxSigma', 'y_base_PsfFlux_flag', 'y_base_PsfFlux_flux',
+ 'y_base_PsfFlux_fluxSigma', 'y_base_SdssShape_flag',
+ 'y_base_SdssShape_psf_xx', 'y_base_SdssShape_psf_xy',
+ 'y_base_SdssShape_psf_yy', 'y_base_SdssShape_xx', 'y_base_SdssShape_xy',
+ 'y_base_SdssShape_yy', 'y_mag', 'y_mag_err', 'y_modelfit_CModel_flux',
+ 'y_modelfit_CModel_fluxSigma', 'z_base_PsfFlux_flag', 'z_base_PsfFlux_flux',
+ 'z_base_PsfFlux_fluxSigma', 'z_base_SdssShape_flag',
+ 'z_base_SdssShape_psf_xx', 'z_base_SdssShape_psf_xy',
+ 'z_base_SdssShape_psf_yy', 'z_base_SdssShape_xx', 'z_base_SdssShape_xy',
+ 'z_base_SdssShape_yy', 'z_mag', 'z_mag_err', 'z_modelfit_CModel_flux',
+ 'z_modelfit_CModel_fluxSigma']
 
- ### Generated with the following function
- ### But we don't actually want to regenerate this each time we run this script
 
 def generate_columns_to_keep():
     import GCRCatalogs
@@ -66,6 +72,7 @@ def generate_columns_to_keep():
                 if isinstance(vi, str):
                     columns_to_keep.append(vi)
 
+    columns_to_keep = list(set(columns_to_keep))
     return columns_to_keep
 
 
