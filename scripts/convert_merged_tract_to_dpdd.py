4cede6ad4283f9c64a3d7802c7871f762f04bd26
Michael Wood-Vasey <wmwv@pitt.edu>
2018-10-01 18:50:52 -0700

    Finish final draft of convert_tract to HDF, FITS script.

--- scripts/convert_merged_tract_to_dpdd.py
+++ scripts/convert_merged_tract_to_dpdd.py
@@ -6,6 +6,7 @@ Output in HDF, FITS, or Parquet"""
 
 import sys
 
+from astropy.table import Table
 import numpy as np
 import pandas as pd
 
@@ -23,77 +24,65 @@ def load_catalog(tract, reader='dc2_coadd_run1.1p'):
     return GCRCatalogs.load_catalog(reader, trim_thistract_config)
 
 
-def convert_tract_to_dpdd_and_save(tract, reader='dc2_coadd_run1.1p'):
+def convert_tract_to_dpdd(tract, reader='dc2_coadd_run1.1p',
+                          key_prefix='object',
+                          verbose=True):
     """Use the GCR reader to load and save DPDD columns for a given tract.
 
     This is done in one function because we're doing this chunk-by-chunk.
-    There is probably some more functional perpsective that might regain some modularity.
+    There is perhaps some more functional perspective that might regain some modularity.
     """
     cat = load_catalog(tract, reader=reader)
-    columns = cat.list_all_native_quantities()
-    tracts_and_patches = cat.available_tracts_and_patches
+    columns = cat.list_all_quantities()
+    columns.extend(['tract', 'patch'])
 
     # tract is an int
     # but patch is a string (e.g., '01' for '0,1')
-    outfile_format = '{base}_tract_{tract:04d}_patch_{patch:s}.{suffix}'
-    for tp in tracts_and_patches:
-        tract = tp['tract']
-        # Reformat '0,1'->'01'
-        patch = ''.join(tp['patch'].split(','))
-        print(['tract == {tract:04d}'.format(**tp),
-               'patch == "{patch:s}"'.format(**tp)])
+    outfile_base_format = '{base}_tract_{tract:04d}_patch_{patch:s}'
+    quantities = cat.get_quantities(columns, return_iterator=True)
+    for q in quantities:
+        if 'tract' not in q or len(q) < 1:
+            continue
 
-        tract_patch_filter = [
-            GCRQuery('tract == {tract:04d}'.format(**tp)),
-            GCRQuery('patch == "{patch:s}"'.format(**tp))]
-        print(tract_patch_filter)
-        quantities = cat.get_quantities(columns, native_filters=tract_patch_filter)
         df = pd.DataFrame.from_dict(q)
 
-        outfile = outfile_format.format(base='dpdd_object', suffix='hdf5',
-                                        **tp)
-        df.to_hdf(outfile)
+        # We implicitly know that we will chunk by tract+patch
+        # So we can take the tract and patch in the first entry as the tract and patch
+        tract, patch = q['tract'][0], q['patch'][0]
+        patch = ''.join(patch.split(','))
+        info = {'base': 'dpdd_object', 'tract': tract, 'patch': patch,
+               'key_prefix': key_prefix}
+        outfile_base = outfile_base_format.format(**info)
 
+        if verbose:
+            print("Writing HDF5 DPDD file for ", tract, patch)
+        key = '{key_prefix:s}_{tract:04d}_{patch:s}'.format(**info)
+        df.to_hdf(outfile_base+'.hdf5', key=key)
 
-def save_trim_cat_to_hdf(tract, outfile):
-    df_iterator = convert_columns_to_dpdd(tract)
-    for df in df_iterator:
-        print(df)
-        df.to_hdf(outfile)
+        # Can't get Parquet support on NERSC set up quite right
+        # Need PyArrow or fastparquet
+        # df.to_parquet(outfile_base+'.parquet')
 
-
-def save_trim_cat_to_fits(tract, outfile):
-    df = convert_columns_to_dpdd(tract)
-    cat = Table(df)
-    cat.write(outfile)
-
-
-def convert_trim_cat_to_dpdd(infile, outfile, clobber=True):
-    cat = pd.read_hdf(infile)
-    
-    # Remove existing outputfile
-    if clobber:
-        if os.path.exists(outfile):
-            os.remove(outfile)
-
-    cat.to_hdf(outfile)
+        if verbose:
+            print("Writing FITS DPDD file for ", tract, patch)
+        Table.from_pandas(df).write(outfile_base+'.fits')
 
 
 if __name__ == "__main__":
     import sys
 
-    clobber = True
-    write_hdf = True
+    from argparse import ArgumentParser, RawTextHelpFormatter
+    usage = """Convert merged_tract_cat object files to HDF5, FITS, Parquet
+    files labeled with DPDD names, and restricted to only the available columns in the DPDD
+    plus tract and patch for convenience.
+    """
+    parser = ArgumentParser(description=usage,
+                            formatter_class=RawTextHelpFormatter)
+    parser.add_argument('tract', type=int, nargs='+',
+                        help='Skymap tract[s] to process.  Default is all.')
+    parser.add_argument('--verbose', default=False, action='store_true')
 
-    write_fits = False
-    write_parquet = False
-
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
+    args = parser.parse_args(sys.argv[1:])
     
-    if outfile == "":
-        print("No outfile specified.")
-        sys.exit()
-        
-    if write_hdf:
-        convert_trim_cat_to_dpdd(infile, outfile, clobber=clobber)
+    for tract in args.tract:
+        convert_tract_to_dpdd(tract)
