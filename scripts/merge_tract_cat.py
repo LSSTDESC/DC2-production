d9a4d748e5e2125a894ac81e14b3e200e67b5fb8
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-10-10 12:29:53 -0700

    fixes for modelfit keys

--- scripts/merge_tract_cat.py
+++ scripts/merge_tract_cat.py
@@ -205,11 +205,11 @@ def load_patch(butler_or_repo, tract, patch,
         cat['mag_err'] = mag_err
         cat['SNR'] = np.abs(cat['base_PsfFlux_flux'])/cat['base_PsfFlux_fluxSigma']
 
-        modelfit_mag, modelfit_mag_err = CoaddCalib.getMagnitude(cat['base_modelfit_CModel_flux'], cat['base_modelfit_CModel_fluxSigma'])
+        modelfit_mag, modelfit_mag_err = CoaddCalib.getMagnitude(cat['modelfit_CModel_flux'], cat['modelfit_CModel_fluxSigma'])
 
-        cat['modelfit_mag'] = mag
-        cat['modelfit_mag_err'] = mag_err
-        cat['modelfit_SNR'] = np.abs(cat['base_modelfit_CModel_flux'])/cat['base_modelfit_CModel_fluxSigma']
+        cat['modelfit_mag'] = modelfit_mag
+        cat['modelfit_mag_err'] = modelfit_mag_err
+        cat['modelfit_SNR'] = np.abs(cat['modelfit_CModel_flux'])/cat['modelfit_CModel_fluxSigma']
 
 
         cat = cat[isPrimary]
