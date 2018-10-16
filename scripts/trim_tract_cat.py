d72d4adb0ed1c9da8bebcc83d023a4dc1748aacc
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-10-16 13:42:58 -0700

    add {}_modelfit_mag, {}_modelfit_mag_err, and {}_modelfit_SNR for u, g, r, i, z, y in trim_tract

--- scripts/trim_tract_cat.py
+++ scripts/trim_tract_cat.py
@@ -56,7 +56,14 @@ columns_to_keep = \
  'z_base_SdssShape_psf_xx', 'z_base_SdssShape_psf_xy',
  'z_base_SdssShape_psf_yy', 'z_base_SdssShape_xx', 'z_base_SdssShape_xy',
  'z_base_SdssShape_yy', 'z_mag', 'z_mag_err', 'z_modelfit_CModel_flux',
- 'z_modelfit_CModel_fluxSigma']
+ 'z_modelfit_CModel_fluxSigma',
+ 'u_modelfit_mag','u_modelfit_mag_err','u_modelfit_SNR',
+ 'g_modelfit_mag','g_modelfit_mag_err','g_modelfit_SNR',
+ 'r_modelfit_mag','r_modelfit_mag_err','r_modelfit_SNR',
+ 'i_modelfit_mag','i_modelfit_mag_err','i_modelfit_SNR',
+ 'z_modelfit_mag','z_modelfit_mag_err','z_modelfit_SNR',
+ 'y_modelfit_mag','y_modelfit_mag_err','y_modelfit_SNR'
+]
 
 
 def generate_columns_to_keep():
