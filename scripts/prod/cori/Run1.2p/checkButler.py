096b6ecc854863871141392257e23ac20065315d
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-10-30 12:31:20 -0700

    add test on isprimary for butler

--- scripts/prod/cori/Run1.2p/checkButler.py
+++ scripts/prod/cori/Run1.2p/checkButler.py
@@ -5,6 +5,12 @@ print(repo)
 from lsst.daf.persistence import Butler
 butler = Butler(repo)
 
+
+tract=4850
+patch='3,4'
 # Test retrieval of a specific tract+patch
-cat = butler.get(datasetType='deepCoadd_ref', dataId={'tract': 4850, 'patch': '3,4'})
-print(cat)
+cat = butler.get(datasetType='deepCoadd_ref', dataId={'tract': tract, 'patch': patch})
+
+
+prim=cat['detect_isPrimary']
+print("#objects={} primaries={} frac={}".format(len(prim),sum(prim),sum(prim)/len(prim)))
