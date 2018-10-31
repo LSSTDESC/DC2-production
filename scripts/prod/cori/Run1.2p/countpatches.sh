0913e7db7050100971a365c98d5e31fd13bd5cd2
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-10-31 07:20:09 -0700

    utility to track the number of patches

---
+++ scripts/prod/cori/Run1.2p/countpatches.sh
@@ -0,0 +1,10 @@
+
+#repo=/global/cscratch1/sd/desc/DC2/data/Run1.1p/Run1.1/output
+#repo=/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_30/rerun/coadd-all2
+repo=/global/cscratch1/sd/desc/DC2/data/Run1.2i/rerun/coadd
+
+echo $repo
+for filter in u g r i z y; do 
+echo ${filter}
+ls $repo/deepCoadd-results/${filter}/* | wc -l 
+done
