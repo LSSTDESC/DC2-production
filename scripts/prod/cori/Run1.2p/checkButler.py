9cbd0101672930e62a120aeb08fadc0bded93440
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-10-31 06:56:48 -0700

    get list of patches from butler

--- scripts/prod/cori/Run1.2p/checkButler.py
+++ scripts/prod/cori/Run1.2p/checkButler.py
@@ -1,5 +1,6 @@
 # And then from Python:
-repo='/global/projecta/projectdirs/lsst/global/in2p3/Run1.2p/w_2018_30/rerun/coadd-all2'
+import os
+repo=os.getenv('DM_REPO')
 print(repo)
 
 from lsst.daf.persistence import Butler
@@ -11,6 +12,9 @@ patch='3,4'
 # Test retrieval of a specific tract+patch
 cat = butler.get(datasetType='deepCoadd_ref', dataId={'tract': tract, 'patch': patch})
 
-
 prim=cat['detect_isPrimary']
 print("#objects={} primaries={} frac={}".format(len(prim),sum(prim),sum(prim)/len(prim)))
+
+skymap = butler.get(datasetType='deepCoadd_skyMap')
+patches = ['%d,%d' % patch.getIndex() for patch in skymap[tract]]
+print(patches)
