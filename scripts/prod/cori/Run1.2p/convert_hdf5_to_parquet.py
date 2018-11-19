4ed6399c3eec4394cee4168994b420fa9cc583c1
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-11-19 19:06:55 +0100

    conversion to parquet by tract

---
+++ scripts/prod/cori/Run1.2p/convert_hdf5_to_parquet.py
@@ -0,0 +1,76 @@
+import os,glob
+import numpy as np
+import pandas as pd
+from astropy.table import Table
+
+
+def correctNans(n,df1,df2):
+    s1=str(df1.dtypes[n])
+    s2=str(df2.dtypes[n])
+    if s1=='bool' and s2=='float64':
+        mask=np.isnan(df2[n])
+        if sum(mask)==len(df2[n]):
+            df2[n]=~df2[n].astype(bool) 
+            print("OK: corrected Nan only column {} to False".format(n))
+            return True
+    if s2=='bool' and s1=='float64':
+        mask=np.isnan(df1[n])
+        if sum(mask)==len(df1[n]):
+            df1[n]=~df1[n].astype(bool) 
+            print("OK: corrected Nan only column {} to False".format(n))
+            return True
+    return False
+
+
+#check datatypes are all consistent
+def sameTypes(df1,df2):
+    assert(df1.dtypes.index.size == df2.dtypes.index.size)
+
+    for n in df1.dtypes.index:
+        s1=str(df1.dtypes[n])
+        s2=str(df2.dtypes[n])
+        if not s1==s2:
+            print("different types for {}: {} vs {}".format(n,s1,s2))
+            assert(correctNans(n,df1,df2))
+    #print("all types checked: OK")
+    return True
+
+
+ff=glob.glob("dpdd_object_tract_*.hdf5")
+print("about to run on {} files".format(len(ff)))
+print(ff, "OK?")
+input()
+
+
+#ref will always be 
+store = pd.HDFStore(ff[0],'r')
+keys = store.keys()
+df_ref=store.get(keys[0])
+store.close()
+
+overwrite=False
+for fin in ff :
+    fout=fin.replace(".hdf5",".fits")
+    #skip if file exists
+    if not overwrite:
+        if os.path.exists(fout):
+        print("{} exists-> skipping".format(fout))
+        continue
+    dfs=[]
+    store = pd.HDFStore(fin,'r')
+    keys = store.keys()
+    print("{}, #patches={}".format(fin,len(keys)))
+    for k in keys:
+        print(k)
+        df=store.get(k)
+        if not sameTypes(df_ref,df):
+            print("WARNING!!!!! inconsistent types in {} {}".format(fin,k))
+        # sort col names for fits writing always the same way
+        df=df[df_ref.dtypes.index]
+        dfs.append(df)
+
+    dftot= pd.concat(dfs, ignore_index=True)
+    print("writing {}".format("test.parquet"))
+    print(dftot.dtypes.index)
+    dftot.to_parquet("test.parquet")
+    store.close()
