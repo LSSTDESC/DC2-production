25f7246650f43689c0b2ed5c5fa3dc905a714d53
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-11-22 01:09:06 -0800

    put proper DM_REOP scratch

--- scripts/prod/cori/Run1.2p/setup.sh
+++ scripts/prod/cori/Run1.2p/setup.sh
@@ -22,7 +22,7 @@ fi
 #DM output directory
 if [ -z "${DM_REPO}" ] ; then
 #export DM_REPO="/global/projecta/projectdirs/lsst/global/in2p3/Run1.2p/w_2018_30/rerun/coadd-all2"
-export DM_REPO=/global/cscratch1/sd/desc/DC2/data/Run1.2p_globus_v2/w_2018_30/rerun/coadd-all2
+export DM_REPO=/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_30/rerun/coadd-all2
 fi
 
 #output DIR
