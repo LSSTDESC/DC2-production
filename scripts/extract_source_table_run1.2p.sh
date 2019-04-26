b92e44713e00c0df74476e5b29b17793210ab87e
Michael Wood-Vasey <wmwv@pitt.edu>
2019-04-26 13:50:59 -0700

    Disable HDF5 file locking to allow running from compute nodes over NFS.

--- scripts/extract_source_table_run1.2p.sh
+++ scripts/extract_source_table_run1.2p.sh
@@ -10,6 +10,9 @@ OUTPUT_DIR=${SCRATCH}/DC2/Run1.2p/src_visit
 #   --base_dir ${BASE_READER_DIR} \
 OBJECT_TABLE=dc2_object_run1.2p
 
+# We can't lock over NFS file system
+export HDF5_USE_FILE_LOCKING=FALSE
+
 mkdir -p ${OUTPUT_DIR}
 
 # DESCPYTHONPATH will get prepended to the PYTHONPATH
