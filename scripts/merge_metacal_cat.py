3ce63c9509f2a64b6371adef84a0fd4626a119f8
Francois <francois@orioncloud.fr>
2019-05-08 15:13:13 -0700

    Fixes bug

--- scripts/merge_metacal_cat.py
+++ scripts/merge_metacal_cat.py
@@ -105,7 +105,7 @@ def load_metacal_patch(butler_or_repo, tract, patch,
     metacal = metacal.asAstropy().to_pandas()
     metacal = metacal[isPrimary]
     # Dropping redundant columns with the main reference catalog
-    metacal = metacal.drop(["coord_ra", "coord_dec", "parent"])
+    metacal = metacal.drop(["coord_ra", "coord_dec", "parent"], axis=1)
 
     # TODO: Remove this when we can create composite catalogs
     metacal = pd.merge(metacal, ref_table, on=fields_to_join, sort=False)
