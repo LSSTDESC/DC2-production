50a04817c454280b462214da8f9b417d643598aa
Michael Wood-Vasey <wmwv@pitt.edu>
2019-06-08 14:40:57 -0700

    Add extract_dia_object test script.
    Fixed for 4849
    Uses Bob Amrstrong diffim_template test repo.

---
+++ scripts/extract_dia_object_table_run1.2p_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# TRACT=$1
+TRACT=4849
+echo "Processing: " $VISIT_FILE
+
+### This repo was prepared with a variant of 2019.13
+REPO=/global/cscratch1/sd/rearmstr/new_templates/diffim_template
+SCRIPT_DIR=${HOME}/local/lsst/DC2-production/scripts
+OUTPUT_DIR=${SCRATCH}/DC2/Run1.2p/dia_object
+
+mkdir -p ${OUTPUT_DIR}
+
+# DESCPYTHONPATH will get prepended to the PYTHONPATH
+# by the sourcing of the setup file
+# Need w.2019.13_diff branch of obs_lsst
+export DESCPYTHONPATH=${HOME}/local/lsst/obs_lsst
+
+# . ${SCRIPT_DIR}/setup_shifter_env.sh
+
+python ${SCRIPT_DIR}/merge_dia_object_cat.py ${REPO} \
+   ${TRACT} \
+   --dataset deepDiff_diaObject \
+   --output_name dia_object \
+   --verbose \
+   --output_dir ${OUTPUT_DIR} \
