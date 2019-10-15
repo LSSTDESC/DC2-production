39ac4806fda35628d14e07c6acc75a82d45efeb9
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-10-15 09:22:34 -0700

    improve cosmodc2

--- scripts/prod/cori/CosmoDC2/cosmoDC2_to_parquet.py
+++ scripts/prod/cori/CosmoDC2/cosmoDC2_to_parquet.py
@@ -9,35 +9,46 @@ from time import time
 sys.path.insert(0,'/global/homes/p/plaszczy/DC2/gcr-catalogs')
 import GCRCatalogs
 
+
+CATALOG='cosmoDC2_v1.1.4_image'
+
 #version GCR
 print('GCRCatalogs =', GCRCatalogs.__version__, '|' ,'GCR =', GCRCatalogs.GCR.__version__)
 #liste des catalogues
-print('\n'.join(sorted(GCRCatalogs.get_available_catalogs())))
-gc = GCRCatalogs.load_catalog('cosmoDC2_v1.1.4_small')
+print('\n'.join(sorted(GCRCatalogs.get_available_catalogs(include_default_only=False))))
+gc = GCRCatalogs.load_catalog(CATALOG)
 print(gc.get_catalog_info('description'))
+#print(', '.join(sorted(gc.list_all_quantities())))                                                    
 
 
 #data=gc.get_quantities([q for q in gc.list_all_quantities()],native_filters=['healpix_pixel == 9813'])
 
-cols=['halo_id','is_central','position_x','position_y','position_z','position_angle_true','ra','dec','redshift','size_true']
-filters=['u','g','r','i','z','Y']
+cols=['halo_id','is_central','stellar_mass','position_x','position_y','position_z','position_angle_true','ra','dec','redshift','size_true']
+filters=['u','g','r','i','z','y']
 for f in filters:
-    s="Mag_true_{0}_lsst_z0,mag_true_{0}_lsst".format(f)
+    s="mag_{0},mag_true_{0},Mag_true_{0}_lsst_z0".format(f)
     cols+=s.split(',')
 
 print(cols)
 
 #output
-parquet_file="xyz_v1.1.4_hive"
+out=CATALOG+".parquet"
+OUTDIR="/global/cscratch1/sd/plaszczy/CosmoDC2"
 
-pix=np.loadtxt("healpix_1.4.4",unpack=True).astype('int')
+#creer output
+OUTDIR=os.path.join(OUTDIR,out)
+#parquet_file=os.path.join(OUTDIR,out)
+assert os.path.exists(OUTDIR)
+
+#pix=np.loadtxt("healpix_1.4.4",unpack=True).astype('int')
+
+#from /global/homes/p/plaszczy/DC2/gcr-catalogs/GCRCatalogs/catalog_configs/cosmoDC2_v1.1.4_image.yaml
+healpix_pixels=[8786, 8787, 8788, 8789, 8790, 8791, 8792, 8793, 8794, 8913, 8914, 8915, 8916, 8917, 8918, 8919, 8920, 8921, 9042, 9043, 9044, 9045, 9046, 9047, 9048, 9049,9050, 9169, 9170, 9171, 9172, 9173, 9174, 9175, 9176, 9177, 9178, 9298, 9299, 9300, 9301, 9302, 9303, 9304, 9305, 9306, 9425, 9426, 9427, 9428, 9429, 9430,9431, 9432, 9433, 9434, 9554, 9555, 9556, 9557, 9558, 9559, 9560, 9561, 9562, 9681, 9682, 9683, 9684, 9685, 9686, 9687, 9688, 9689, 9690, 9810, 9811, 9812,9813, 9814, 9815, 9816, 9817, 9818, 9937, 9938, 9939, 9940, 9941, 9942, 9943, 9944, 9945, 9946, 10066, 10067, 10068, 10069, 10070, 10071, 10072, 10073, 10074, 10193,10194, 10195, 10196, 10197, 10198, 10199, 10200, 10201, 10202, 10321, 10322, 10323, 10324, 10325, 10326, 10327, 10328, 10329, 10444, 10445, 10446, 10447, 10448, 10449, 10450,10451, 10452]
+
+
+
 nskip=0
-for ipix in pix:
-    ## parquet_file="xyz_{}.parquet".format(ipix)
-    ## if os.path.exists(parquet_file):
-    ##     nskip+=1
-    ##     print("skipping {}: {}".format(parquet_file,nskip))
-    ##     continue
+for ipix in healpix_pixels:
     print(ipix)
     t0=time()
     data=gc.get_quantities(cols,native_filters=["healpix_pixel == {}".format(ipix)])
@@ -49,8 +60,11 @@ for ipix in pix:
         if str(df.dtypes[n])=='float64':
             df[n]=df[n].astype('float32') 
     #writing
-    df.to_parquet(parquet_file,append=os.path.exists(parquet_file),
-                      file_scheme='hive',engine='fastparquet',compression=None)
+    #df.to_parquet(parquet_file,append=os.path.exists(parquet_file),
+    #                  file_scheme='hive',engine='fastparquet',compression=None)
+
+    parquet_file=os.path.join(OUTDIR,str(ipix))
+    df.to_parquet(parquet_file,engine='fastparquet',compression=None)
     t2=time()
     print("Wrote to {}".format(parquet_file))
     print("Tot time to process {:2.1f}s".format(t2-t0))
