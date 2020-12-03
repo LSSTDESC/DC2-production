cba81981738e706aa239ba85241f9f2a3204c500
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-12-03 08:36:24 -0500

    refactor code

--- scripts/merge_truth_per_tract.py
+++ scripts/merge_truth_per_tract.py
@@ -12,26 +12,31 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 import pandas as pd
 
-__all__ = ["merge_truth_per_tract"]
+__all__ = ["merge_truth_per_tract", "match_object_with_merged_truth"]
 
 
-def merge_truth_per_tract(
-    input_dir,
-    name,
-    output_dir,
-    object_catalog_path=None,
-    validate=False,
-    silent=False,
-    **kwargs
-):
-    """
-    TODO: add docstring
+def merge_truth_per_tract(input_dir, validate=False, silent=False, **kwargs):
+    """Merge all the truth catalogs in one single tract directory
+
+    Parameters
+    ----------
+    input_dir : str
+        Path to the input tract directory
+
+    Optional Parameters
+    ----------------
+    validate : bool, optional (default: False)
+        If true, check the tract column has only one value
+    silent : bool, optional (default: False)
+        If true, turn off most printout.
     """
     my_print = (lambda x: None) if silent else print
 
+    my_print("Merging all files in", input_dir)
     files_to_merge = sorted(os.listdir(input_dir))
     df_to_merge = []
     for filename in files_to_merge:
+        my_print("Loading", filename)
         df = pd.read_parquet(os.path.join(input_dir, filename))
         m = re.search(r"_hp(\d+)\b", filename)
         if m is not None:
@@ -47,49 +52,89 @@ def merge_truth_per_tract(
     df = pd.concat(df_to_merge, ignore_index=True)
     del df_to_merge
 
-    tract = df.loc[0, "tract"]
-    output_path = os.path.join(output_dir, "{}_tract{}.parquet".format(name, tract))
+    if validate:
+        assert len(np.unique(df["tract"].values)) == 1
 
-    if not object_catalog_path:
-        my_print("No object catalog given! No matching is done!")
-        df.to_parquet(output_path, index=False)
-        return
+    return df
 
-    object_cat = pd.read_parquet(object_catalog_path, columns=["objectId", "ra", "dec"])
 
+def match_object_with_merged_truth(truth_cat, object_cat, validate=False, silent=False, **kwargs):
+    """Match object catalog with truth catalog
+
+    Parameters
+    ----------
+    truth_cat : str or pd.DataFrame
+        Truth catalog or its path
+    object_cat : str or pd.DataFrame
+        Object catalog or its path
+
+    Optional Parameters
+    ----------------
+    validate : bool, optional (default: False)
+        If true, check the matching is perform properly.
+    silent : bool, optional (default: False)
+        If true, turn off most printout.
+    """
+    my_print = (lambda x: None) if silent else print
+
+    if isinstance(truth_cat, str):
+        my_print("Loading truth catalog from", truth_cat)
+        truth_cat = pd.read_parquet(truth_cat)
+
+    if isinstance(object_cat, str):
+        my_print("Loading object catalog from", object_cat)
+        object_cat = pd.read_parquet(object_cat, columns=["objectId", "ra", "dec"])
+
+    my_print("Performing nearest neighbor match")
     object_sc = SkyCoord(object_cat["ra"].values, object_cat["dec"].values, unit="deg")
-    truth_sc = SkyCoord(df["ra"].values, df["dec"].values, unit="deg")
+    truth_sc = SkyCoord(truth_cat["ra"].values, truth_cat["dec"].values, unit="deg")
     idx, sep, _ = object_sc.match_to_catalog_sky(truth_sc)
     del object_sc, truth_sc
 
-    matched = pd.DataFrame.from_dict({"idx": idx, "sep": sep.arcsec, "objectId": object_cat["objectId"]})
+    my_print("Merging matching info with truth catalog")
+    matched = pd.DataFrame.from_dict({
+        "truth_idx": truth_cat.index.values[idx],
+        "match_sep": sep.arcsec,
+        "match_objectId": object_cat["objectId"]
+    })
     del idx, sep
 
-    matched.sort_values("sep", inplace=True)
-    matched["is_unique_truth_entry"] = ~matched.duplicated("idx", keep="first")
+    matched.sort_values("match_sep", inplace=True)
+    matched["is_unique_truth_entry"] = ~matched.duplicated("truth_idx", keep="first")
     matched.sort_index(inplace=True)
 
-    not_matched_truth_idx = np.flatnonzero(np.in1d(df.index.values, matched["idx"].values, invert=True))
-    not_matched = pd.DataFrame.from_dict({"idx": not_matched_truth_idx, "sep": -1.0, "objectId": -1, "is_unique_truth_entry": True})
+    not_matched_truth_idx = truth_cat.index.values[np.in1d(truth_cat.index.values, matched["truth_idx"].values, invert=True)]
+    not_matched = pd.DataFrame.from_dict({
+        "truth_idx": not_matched_truth_idx,
+        "match_sep": -1.0,
+        "match_objectId": -1,
+        "is_unique_truth_entry": True
+    })
 
     full_table = pd.concat([matched, not_matched], ignore_index=True)
     del matched, not_matched, not_matched_truth_idx
 
-    full_table = pd.merge(full_table, df, left_on="idx", right_index=True, how="outer", validate="many_to_one")
+    full_table = pd.merge(truth_cat, full_table, left_index=True, right_on="truth_idx", how="outer")
     full_table.sort_index(inplace=True)
 
     if validate:
-        objid = full_table["objectId"].values
+        objid = full_table["match_objectId"].values
         assert (objid[objid > -1] == object_cat["objectId"].values).all()
         del objid
-        truth_idx = full_table["idx"].values[full_table["is_unique_truth_entry"].values]
+        truth_idx = full_table["truth_idx"].values[full_table["is_unique_truth_entry"].values]
         truth_idx.sort()
-        assert (truth_idx == df.index.values).all()
+        assert (truth_idx == truth_cat.index.values).all()
         del truth_idx
 
-    del full_table["idx"], df, object_cat
+    del full_table["truth_idx"]
 
-    full_table.to_parquet(output_path, index=False)
+    return full_table
+
+
+def save_df_to_disk(df, output_dir, name="truth", **kwargs):
+    tract = df.loc[0, "tract"]
+    output_path = os.path.join(output_dir, "{}_tract{}.parquet".format(name, tract))
+    df.to_parquet(output_path, index=False)
 
 
 def main():
@@ -106,7 +151,12 @@ def main():
     parser.add_argument("--validate", action="store_true")
     parser.add_argument("--silent", action="store_true")
 
-    merge_truth_per_tract(**vars(parser.parse_args()))
+    args = parser.parse_args()
+
+    df = merge_truth_per_tract(**vars(args))
+    if args.object_catalog_path:
+        df = match_object_with_merged_truth(df, args.object_catalog_path, **vars(args))
+    save_df_to_disk(df, **vars(args))
 
 
 if __name__ == "__main__":
